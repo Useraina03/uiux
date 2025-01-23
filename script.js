572ffbf38de6c29bafb35c49e3d6909271a1e29a
useraina03 <fanaticm121@gmail.com>
2025-01-24 02:43:46 +0530

    1st update

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+
+const container = document.querySelector('.container');
+const btns = document.querySelectorAll('.btn');
+const imgList = ["1","2","3","4"];
+let index=0;
+
+btns.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        if(button.classList.contains('btn-left')){
+            index--;
+            if (index<0){
+                index = imgList.length-1;
+            }
+            container.style.background = `url("img/${imgList[index]}.jpg") center/cover fixed no-repeat`
+
+        }
+        else{
+            index ++;
+            if(index===imgList.length){
+                index =0;
+
+            }
+            container.style.background=`url("img/${imgList[index]}.jpg") center/cover fixed no-repeat`
+        }
+    });
+});
+
 document.querySelector('.menu-toggle').addEventListener('click',()=>{
     document.querySelector('.nav-links').classList.toggle('active')
 });
@@ -5,3 +32,4 @@ document.getElementById('contactForm').addEventListener('submit',(e)=>{
     e.preventDefault();
     alert("Thank you")
 })
+
